b79e91a99ce3038d3ae223cfa4536268ea74cc83
jinronga <1460595002@qq.com>
2024-08-05 17:23:13 +0800

    fix: Rename AlarmGroup name comment

--- pkg/palace/model/bizmodel/alarm_group.go
+++ pkg/palace/model/bizmodel/alarm_group.go
@@ -12,7 +12,7 @@ const tableNameAlarmGroup = "alarm_group"
 // AlarmGroup 告警组
 type AlarmGroup struct {
 	model.AllFieldModel
-	Name    string           `gorm:"column:name;type:varchar(64);not null;uniqueIndex:idx__name,priority:1;comment:规则组名称"`
+	Name    string           `gorm:"column:name;type:varchar(64);not null;uniqueIndex:idx__name,priority:1;comment:告警组名称"`
 	Status  vobj.Status      `gorm:"column:status;type:tinyint;not null;default:1;comment:启用状态1:启用;2禁用"`
 	Remark  string           `gorm:"column:remark;type:varchar(255);not null;comment:描述信息"`
 	Members []*SysTeamMember `gorm:"many2many:alarm_group_members" json:"members"`
